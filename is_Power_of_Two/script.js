d808b0ad985f38f8d1104b6c19ec293a62a6299e
Alfat <alfattasnimhasan@gmail.com>
2026-08-17 00:33:34 +0600

    is the number power of two or not solved

---
+++ is_Power_of_Two/script.js
@@ -0,0 +1,13 @@
+const isPowerOfTwo = (num) => {
+  let result = false;
+  for (let i = 1; i < num; i++) {
+    if (2 ** i === num) {
+      result = true;
+    }
+  }
+  return result;
+};
+
+console.log(isPowerOfTwo(8));
+console.log(isPowerOfTwo(7));
+console.log(isPowerOfTwo(64));
