3530b71f2ac4688240883c8888ba5faa06a9323f
Alfat <alfattasnimhasan@gmail.com>
2026-08-27 00:37:57 +0600

    random hex color generate

---
+++ Random_Hex_Color_Generator/script.js
@@ -0,0 +1,8 @@
+const randomHexColor = () => {
+  let randomNumber = Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0");
+  return `#${randomNumber}`;
+};
+
+console.log(randomHexColor());
